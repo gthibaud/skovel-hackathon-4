a47d9f5523a6dba4a9664f44c90e2f1336bdbaf5
Grégoire Thibaud <gregoire.thibo@gmail.com>
2024-03-30 20:04:44 +0100

    enhance sitemap generation

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,20 @@
+import { MenuItem, menu } from '@/data/menu';
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
+    const generateMenuItemSitemap = (item: MenuItem): MetadataRoute.Sitemap => {
+        const items = item.items || [];
+        return [
+            {
+                url: `https://capybarun.skovel.com${item.to}`,
+                lastModified: new Date(),
+                changeFrequency: 'monthly',
+                priority: 0.8,
+            },
+            ...items.flatMap(generateMenuItemSitemap),
+        ];
+    };
+
     return [
         {
             url: 'https://capybarun.skovel.com/',
@@ -8,17 +22,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
             changeFrequency: 'monthly',
             priority: 1,
         },
-        {
-            url: 'https://capybarun.skovel.com/about',
-            lastModified: new Date(),
-            changeFrequency: 'monthly',
-            priority: 0.8,
-        },
-        {
-            url: 'https://capybarun.skovel.com/register',
-            lastModified: new Date(),
-            changeFrequency: 'monthly',
-            priority: 0.5,
-        },
+        ...menu.flatMap(generateMenuItemSitemap),
     ];
 }
